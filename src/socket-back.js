119eaefbdeef5f12efd593541af557e4238d18e2
Makinay <vitor_souza97@hotmail.com>
2023-01-09 19:00:23 -0300

    comunicação feita sem necessidade de console finalizado

--- src/socket-back.js
+++ src/socket-back.js
@@ -3,8 +3,7 @@ import io from "./index.js"
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID: ", socket.id)
 
-    socket.on('texto_editor', (texto) => {
-        console.log(texto)
-        io.emit('texto_editor_clientes', texto)
+    socket.on('texto_editor', (texto) => {        
+        socket.broadcast.emit('texto_editor_clientes', texto)
     })
 })
