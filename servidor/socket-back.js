00a05713f38e5b4a9e72d74ae323df1923b0d63a
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-07-04 19:39:25 -0300

    refatorado o projeto criando arquivos para registrar os eventos para facil manutenção

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,11 +1,6 @@
-import {
-  adicionarDocumento,
-  atualizaDocumento,
-  encontrarDocumento,
-  excluirDocumento,
-  obterDocumentos,
-} from "./db/documentosDB.js";
 import io from "./index.js";
+import registrarEventosDocumento from "./registrarEventos/registrarEventosDocumento.js";
+import registrarEventosInicio from "./registrarEventos/registrarEventosInicio.js";
 
 // eslint-disable-next-line no-unused-vars
 const documentos = [
@@ -24,49 +19,6 @@ const documentos = [
 ];
 
 io.on("connection", (socket) => {
-  socket.on("obter_documentos", async (devolverDocumentos) => {
-    const documentos = await obterDocumentos();
-
-    devolverDocumentos(documentos);
-  });
-
-  socket.on("adicionar_documento", async (nome) => {
-    const documentoExiste = (await encontrarDocumento(nome)) !== null;
-
-    if (documentoExiste) {
-      socket.emit("documento_existente", nome);
-    } else {
-      const resultado = await adicionarDocumento(nome);
-
-      if (resultado.acknowledged) {
-        io.emit("adicionar_documento_interface", nome);
-      }
-    }
-  });
-
-  socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
-    socket.join(nomeDocumento);
-
-    const documento = await encontrarDocumento(nomeDocumento);
-
-    if (documento) {
-      devolverTexto(documento.texto);
-    }
-  });
-
-  socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
-    const atualizacao = await atualizaDocumento(nomeDocumento, texto);
-
-    if (atualizacao.modifiedCount) {
-      socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
-    }
-  });
-
-  socket.on("excluir_documento", async (nome) => {
-    const resultado = await excluirDocumento(nome);
-
-    if (resultado.deletedCount) {
-      io.emit("excluir_documento_sucesso", nome);
-    }
-  });
+  registrarEventosInicio(socket, io);
+  registrarEventosDocumento(socket, io);
 });
